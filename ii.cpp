1d77f6aa1f8f5a46d55043ed26f5b24f56683c1c
Nimisha202098 <90142133+Nimishaaaaa@users.noreply.github.com>
2021-09-30 23:26:04 +0530

    Update ii.cpp

--- ii.cpp
+++ ii.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 #define SIZE 10
 using namespace std;
+
+
+void Binarysearch(int arr[], int size, int elemwnt)
+{ 
+  int start=0, last=size-1;
+  int mid=(start+last)/2;
+  
+  while(start<= last)
+    
+    if( arr[mid] < element)
+      start=mid+1;
+  else if( arr[mid] == element)
+    
+  {
+    count<<element<<" exist in index" <<mid<<end;
+    break;
+  }
+  
+  if(start>last)
+    count<<" element does not exist in index "<<end;
+}
+
+void printArray(int arr[],
